f5aa328587dc458a9839521fac689b263b9e7d72
ArjayAquino <90899352+h4rshjo@users.noreply.github.com>
2024-07-14 19:55:41 +0800

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,42 +1,58 @@
-// SPDX-License-Identifier: MIT
-// This line specifies the license type for the contract, in this case, MIT license.
-
-pragma solidity ^0.8.17;
-// This line specifies the version of Solidity compiler to be used for compiling this contract.
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
 
 contract Assessment {
-    // Declare a state variable 'balance' of type uint256 to store the contract balance.
+    address payable public owner;
     uint256 public balance;
 
-    // Constructor function that runs once when the contract is deployed.
-    // It is marked 'payable' to allow the contract to receive Ether upon deployment.
-    constructor() payable {
-        // Initialize the contract balance with the Ether sent during deployment.
-        balance = msg.value;
+    event Deposit(uint256 amount);
+    event Withdraw(uint256 amount);
+
+    constructor(uint initBalance) payable {
+        owner = payable(msg.sender);
+        balance = initBalance;
     }
 
-    // Function to get the current balance of the contract.
-    // It is marked as 'view' because it does not modify the state.
-    function getBalance() public view returns (uint256) {
+    function getBalance() public view returns(uint256) {
         return balance;
     }
 
-    // Function to deposit Ether into the contract.
-    // It is marked as 'payable' to allow the function to receive Ether.
-    function deposit() public payable {
-        // Increment the balance by the amount of Ether sent in the transaction.
-        balance += msg.value;
+    function deposit(uint256 _amount) public payable {
+        uint _previousBalance = balance;
+
+        // make sure this is the owner
+        require(msg.sender == owner, "You are not the owner of this account");
+
+        // perform transaction
+        balance += _amount;
+
+        // assert transaction completed successfully
+        assert(balance == previousBalance + amount);
+
+        // emit the event
+        emit Deposit(_amount);
     }
 
-    // Function to withdraw a specified amount of Ether from the contract.
-    function withdraw(uint256 amount) public {
-        // Ensure that the contract has enough balance to fulfill the withdrawal request.
-        require(balance >= amount, "Insufficient balance");
+    // custom error
+    error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
 
-        // Decrement the balance by the amount to be withdrawn.
-        balance -= amount;
+    function withdraw(uint256 _withdrawAmount) public {
+        require(msg.sender == owner, "You are not the owner of this account");
+        uint _previousBalance = balance;
+        if (balance < _withdrawAmount) {
+            revert InsufficientBalance({
+                balance: balance,
+                withdrawAmount: _withdrawAmount
+            });
+        }
 
-        // Transfer the specified amount of Ether to the caller of the function.
-        payable(msg.sender).transfer(amount);
+        // withdraw the given amount
+        balance -= _withdrawAmount;
+
+        // assert the balance is correct
+        assert(balance == (previousBalance - withdrawAmount));
+
+        // emit the event
+        emit Withdraw(_withdrawAmount);
     }
 }
